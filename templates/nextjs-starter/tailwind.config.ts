9806792e75b3110b734abe054a00c3f415b2057d
David Dal Busco <david.dalbusco@outlook.com>
2024-04-05 07:33:36 +0200

    feat: starter page

--- templates/nextjs-starter/tailwind.config.ts
+++ templates/nextjs-starter/tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
+import {fontFamily} from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: ['JetBrains Mono', 'sans-serif', ...fontFamily.sans]
     },
-  },
-  plugins: [],
+    colors: {
+      inherit: 'inherit',
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: 'rgb(0 0 0)',
+      white: 'rgb(255 255 255)',
+      ['lavender-blue']: {
+        50: '#f2f3ff',
+        100: '#e4e7ff',
+        200: '#c9cfff',
+        300: '#aeb8ff',
+        400: '#93a0ff',
+        500: '#7888ff',
+        600: '#606dcc',
+        700: '#485299',
+        800: '#303666',
+        900: '#181b33'
+      }
+    }
+  }
 };
 export default config;
