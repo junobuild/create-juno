85f9e763b72ec7881af890f1bce35d6d579ad5be
David Dal Busco <david.dalbusco@outlook.com>
2024-04-09 21:45:46 +0200

    chore: no dev

--- templates/astro-starter/astro.config.mjs
+++ templates/astro-starter/astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import juno from "@junobuild/vite-plugin";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
