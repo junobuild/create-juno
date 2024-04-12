5829f6b38f34d9f204654792c40fd8f210e4a037
David Dal Busco <david.dalbusco@outlook.com>
2024-04-12 16:27:18 +0200

    refactor: move cmp

---
+++ templates/angular-starter/src/app/components/home/home.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {ArticleComponent} from '../article/article.component';
+import {BackgroundComponent} from '../background/background.component';
+import {FooterComponent} from '../footer/footer.component';
+import {HeroComponent} from '../hero/hero.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HeroComponent, ArticleComponent, FooterComponent, BackgroundComponent],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {}
