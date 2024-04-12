e87403219666f69c104a3a6d588706c3854ff906
David Dal Busco <david.dalbusco@outlook.com>
2024-04-12 08:26:02 +0200

    feat: integrate react starter

--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -1,5 +1,9 @@
 import type {Template} from '../types/template';
 
+const APP_DESCRIPTION_STARTER = 'Opt for a barebones scaffolding to kickstart your app';
+const APP_DESCRIPTION_EXAMPLE =
+  'Begin with an example app featuring authentication, document storage, and image handling';
+
 export const TEMPLATES: Template[] = [
   {
     framework: `Astro`,
@@ -12,23 +16,28 @@ export const TEMPLATES: Template[] = [
     framework: `Next.js`,
     key: `nextjs-starter`,
     type: 'Starter',
-    description: 'Opt for a barebones scaffolding to kickstart your app',
+    description: APP_DESCRIPTION_STARTER,
     kind: 'app'
   },
   {
     framework: `Next.js`,
     key: `nextjs-example`,
     type: 'Example',
-    description:
-      'Begin with an example app featuring authentication, document storage, and image handling',
+    description: APP_DESCRIPTION_EXAMPLE,
+    kind: 'app'
+  },
+  {
+    framework: `React`,
+    key: `react-starter`,
+    type: 'Starter',
+    description: APP_DESCRIPTION_STARTER,
     kind: 'app'
   },
   {
     framework: `React`,
     key: `react-example`,
     type: 'Example',
-    description:
-      'Begin with an example app featuring authentication, document storage, and image handling',
+    description: APP_DESCRIPTION_EXAMPLE,
     kind: 'app'
   }
 ];
