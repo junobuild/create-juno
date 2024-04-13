773bab7d69932557d5faf2aaf466ddc7fc26dd51
David Dal Busco <david.dalbusco@outlook.com>
2024-04-13 14:04:49 +0200

    feat: config

--- templates/sveltekit-starter/vite.config.ts
+++ templates/sveltekit-starter/vite.config.ts
@@ -3,5 +3,24 @@ import { defineConfig } from 'vite';
 import juno from '@junobuild/vite-plugin';
 
 export default defineConfig({
-	plugins: [sveltekit(), juno({ container: true })]
+	plugins: [sveltekit(), juno({ container: true })],
+	optimizeDeps: {
+		esbuildOptions: {
+			define: {
+				global: 'globalThis'
+			},
+			plugins: [
+				{
+					name: 'fix-node-globals-polyfill',
+					setup(build) {
+						build.onResolve({ filter: /_virtual-process-polyfill_\.js/ }, ({ path }) => ({ path }));
+					}
+				}
+			]
+		}
+	},
+	worker: {
+		plugins: () => [sveltekit()],
+		format: 'es'
+	}
 });
