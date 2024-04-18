86754927286c4a8ba7bafd3cef5624785e3bc6bd
David Dal Busco <david.dalbusco@outlook.com>
2024-04-18 11:58:07 +0200

    fix: href binding

--- templates/vue-starter/src/components/Article.vue
+++ templates/vue-starter/src/components/Article.vue
@@ -8,10 +8,10 @@ defineProps<{
 
 <template>
   <a
-    href="{{href}}"
+    :href="href"
     rel="noreferrer noopener"
     target="_blank"
-    aria-label="{{ariaLabel}}"
+    :aria-label="ariaLabel"
     class="group flex flex-col py-3 px-4 border-black dark:border-lavender-blue-500 dark:hover:border-white border-[3px] rounded bg-white dark:bg-black dark:text-white transition-all shadow-[8px_8px_0px_rgba(0,0,0,1)] dark:shadow-[8px_8px_0px_#7888FF] dark:hover:shadow-[8px_8px_0px_#fff] hover:bg-lavender-blue-200 dark:hover:bg-black active:bg-lavender-blue-400 dark:active:bg-black active:shadow-none active:translate-x-[8px] active:translate-y-[8px]"
   >
     <h4
