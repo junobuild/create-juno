6b16ae00a75d906c64bc7ea73563f72ada36cbe2
David Dal Busco <david.dalbusco@outlook.com>
2024-05-16 21:49:14 +0200

    feat: list

--- templates/vanilla-js-example/src/components/content.js
+++ templates/vanilla-js-example/src/components/content.js
@@ -1,8 +1,11 @@
 import {renderLogout} from './logout';
 import {renderModal} from './modal';
+import {renderTable} from './table';
 
 export const renderContent = (app) => {
   app.innerHTML = `<div>
+
+    ${renderTable(app)}
 
     ${renderModal(app)}
 
