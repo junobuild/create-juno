6ac899721ec3c70330a95758a4852c63235ac008
David Dal Busco <david.dalbusco@outlook.com>
2024-10-24 18:56:53 +0200

    feat: svelte v5 templates (#43)
    * feat: svelte v5 templates
    
    * feat: migrate svelte v5
    
    * feat: migrate svelte v5
    
    * chore: lint
    
    * chore: reload

--- templates/sveltekit-example/src/routes/+layout.svelte
+++ templates/sveltekit-example/src/routes/+layout.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import type { Snippet } from 'svelte';
 	import { initSatellite } from '@junobuild/core-peer';
 	import Footer from '$lib/components/Footer.svelte';
 	import Background from '$lib/components/Background.svelte';
 	import '../app.css';
 	import Auth from '$lib/components/Auth.svelte';
 
-	onMount(
-		async () =>
-			await initSatellite({
-				workers: {
-					auth: true
-				}
-			})
-	);
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	const init = async () => {
+		await initSatellite({
+			workers: {
+				auth: true
+			}
+		});
+	};
+
+	$effect(() => {
+		init();
+	});
 </script>
 
 <div class="relative isolate min-h-[100dvh]">
@@ -29,7 +38,7 @@
 		</p>
 
 		<Auth>
-			<slot />
+			{@render children()}
 		</Auth>
 	</main>
 
