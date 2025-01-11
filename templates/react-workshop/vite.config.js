1fe200b639d94a6c2040874ef7ff55d6a00e1c49
David Dal Busco <david.dalbusco@outlook.com>
2025-01-11 20:56:47 +0100

    fix: polyfill global for agent-js (#60)

--- templates/react-workshop/vite.config.js
+++ templates/react-workshop/vite.config.js
@@ -1,8 +1,9 @@
 import juno from '@junobuild/vite-plugin';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), juno({container: true})]
+  plugins: [react(), nodePolyfills(), juno({container: true})]
 });
--- templates/vue-example/vite.config.ts
+++ templates/vue-example/vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import juno from '@junobuild/vite-plugin'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), juno({ container: true })],
+  plugins: [vue(), vueJsx(), nodePolyfills(), juno({ container: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
