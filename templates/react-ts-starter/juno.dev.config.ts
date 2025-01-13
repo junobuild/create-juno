1fffef15e5cff3b72f4e6daf9c47e5eac528d647
David Dal Busco <david.dalbusco@outlook.com>
2025-01-13 21:23:46 +0100

    feat: ts

--- templates/react-ts-starter/juno.dev.config.ts
+++ templates/react-ts-starter/juno.dev.config.ts
@@ -1,6 +1,5 @@
 import {defineDevConfig} from '@junobuild/config';
 
-/** @type {import('@junobuild/config').JunoDevConfig} */
 export default defineDevConfig(() => ({
   satellite: {
     collections: {
