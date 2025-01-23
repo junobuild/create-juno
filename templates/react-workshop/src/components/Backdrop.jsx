2aa92d6f3f44422e93ca22ef019c98741a357bc6
David Dal Busco <david.dalbusco@outlook.com>
2025-01-23 08:17:22 +0100

    feat: tailwind 4 in react

--- templates/react-workshop/src/components/Backdrop.jsx
+++ templates/react-workshop/src/components/Backdrop.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 export const Backdrop = ({spinner = false}) => (
   <div
-    className={`fixed inset-0 z-40 ${spinner ? 'backdrop-blur-sm' : 'backdrop-blur-xl'} bg-white/30 flex items-center justify-center`}>
+    className={`fixed inset-0 z-40 ${spinner ? 'backdrop-blur-xs' : 'backdrop-blur-xl'} bg-white/30 flex items-center justify-center`}>
     {spinner && (
       <div
         className="w-12 h-12 rounded-full animate-spin
