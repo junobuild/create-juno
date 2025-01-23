8fb330d63ae3cd444aabe9e03d62de2acf1e953f
David Dal Busco <david.dalbusco@outlook.com>
2025-01-23 08:18:51 +0100

    feat: tall and spacing

--- templates/sveltekit-example/src/lib/components/Background.svelte
+++ templates/sveltekit-example/src/lib/components/Background.svelte
@@ -1,5 +1,5 @@
 <div
-	class="hidden sm:block fixed top-[400px] tall:top-2/4 right-0 -z-10 translate-x-2/4 lg:translate-x-1/3 -translate-y-2/4 min-h-[800px] h-[100dvh] aspect-square"
+	class="hidden sm:block fixed top-[400px] [@media(min-height:800px)]:top-2/4 right-0 -z-10 translate-x-2/4 lg:translate-x-1/3 -translate-y-2/4 min-h-[800px] h-[100dvh] aspect-square"
 >
 	<img
 		src="/juno_illustration.svg"
