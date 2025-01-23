9f4af526c3e774b3ba593179d5510a360a1a52e9
David Dal Busco <david.dalbusco@outlook.com>
2025-01-23 08:04:11 +0100

    feat: tall and other changes

--- templates/angular-example/tailwind.config.js
+++ templates/angular-example/tailwind.config.js
@@ -8,9 +8,6 @@ module.exports = {
       sans: ['JetBrains Mono', 'sans-serif', ...fontFamily.sans]
     },
     extend: {
-      screens: {
-        tall: {raw: '(min-height: 800px)'}
-      },
       animation: {
         fade: 'fadeIn .25s ease-in-out'
       },
