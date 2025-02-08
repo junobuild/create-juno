f8061bb955e74d897875e00d38514f4e4c0343c7
jfgrissom <jfgrissom@gmail.com>
2025-02-08 10:28:44 -0800

    feat: add react-ts-example template

---
+++ templates/react-ts-example/src/components/Backdrop.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+
+interface BackdropProps {
+  spinner?: boolean
+}
+
+export const Backdrop: FC<BackdropProps> = (props) => {
+  const { spinner = false } = props
+
+  return (
+    <div
+      className={`fixed inset-0 z-40 ${spinner ? 'backdrop-blur-sm' : 'backdrop-blur-xl'} bg-white/30 flex items-center justify-center`}>
+      {spinner && (
+        <div
+          className="w-12 h-12 rounded-full animate-spin
+          border-[3px] border-solid border-lavender-blue-600 border-t-transparent"></div>
+      )}
+    </div>
+  )
+}
