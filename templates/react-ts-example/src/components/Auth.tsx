6bd335c0a132610594c5591a5620d84b26419270
Jay F. Grissom <jfgrissom@users.noreply.github.com>
2025-02-08 22:53:37 -0800

    Add react-ts-example (#82)
    * feat: add react-ts-example template
    
    * style: adjust names for react-ts packages

---
+++ templates/react-ts-example/src/components/Auth.tsx
@@ -0,0 +1,31 @@
+import { authSubscribe, User } from '@junobuild/core'
+import { FC, PropsWithChildren, createContext, useEffect, useState } from 'react'
+import { Login } from './Login'
+import { Logout } from './Logout'
+
+export const AuthContext = createContext<{ user: User | null }>({ user: null })
+
+export const Auth: FC<PropsWithChildren> = (props) => {
+  const { children } = props
+  const [user, setUser] = useState<User | null>(null)
+
+  useEffect(() => {
+    const sub = authSubscribe((user) => setUser(user))
+
+    return () => sub()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {user !== null ? (
+        <div>
+          {children}
+
+          <Logout />
+        </div>
+      ) : (
+        <Login />
+      )}
+    </AuthContext.Provider>
+  )
+}
