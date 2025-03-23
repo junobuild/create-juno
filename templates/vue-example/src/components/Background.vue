b121354d338a126d6b9eb1e4ec61138e73489234
David Dal Busco <david.dalbusco@outlook.com>
2025-03-23 21:19:03 +0100

    chore: fmt

--- templates/vue-example/src/components/Background.vue
+++ templates/vue-example/src/components/Background.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="tall:top-2/4 fixed top-[400px] right-0 -z-10 hidden aspect-square h-[100dvh] min-h-[800px] -translate-y-2/4 translate-x-2/4 sm:block lg:translate-x-1/3"
+    class="tall:top-2/4 fixed top-[400px] right-0 -z-10 hidden aspect-square h-[100dvh] min-h-[800px] translate-x-2/4 -translate-y-2/4 sm:block lg:translate-x-1/3"
   >
     <img
       src="/juno_illustration.svg"
