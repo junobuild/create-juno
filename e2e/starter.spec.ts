fd66f279e2de3449617a828c6d30e4b2d464eef5
David Dal Busco <david.dalbusco@outlook.com>
2025-03-24 08:45:55 +0100

    test: update links

--- e2e/starter.spec.ts
+++ e2e/starter.spec.ts
@@ -24,34 +24,43 @@ test('has title', async ({page}) => {
   await expect(page).toHaveTitle(`Juno / ${capitalizeAll(TEMPLATE)}`);
 });
 
-test('get quickstart link', async ({ page }) => {
+test('get quickstart link', async ({page}) => {
   await page.goto('/');
 
   const link = page.locator('a[aria-label="Open quickstart guides on Juno\'s website"]');
 
-  await expect(link).toHaveAttribute('href', 'https://juno.build/docs/guides/astro');
+  const framework = TEMPLATE.split("-")[0];
+
+  await expect(link).toHaveAttribute('href', `https://juno.build/docs/guides/${framework}`);
 });
 
-test('get documentation link', async ({ page }) => {
+test('get documentation link', async ({page}) => {
   await page.goto('/');
 
-  const link = page.locator('a[aria-label="Open the list of features for building apps on Juno\'s website"]');
+  const link = page.locator(
+    'a[aria-label="Open the list of features for building apps on Juno\'s website"]'
+  );
 
   await expect(link).toHaveAttribute('href', 'https://juno.build/docs/category/build');
 });
 
-test('get ci link', async ({ page }) => {
+test('get ci link', async ({page}) => {
   await page.goto('/');
 
   const link = page.locator('a[aria-label="Open the guide to setting up GitHub Actions for Juno"]');
 
-  await expect(link).toHaveAttribute('href', 'https://juno.build/docs/miscellaneous/github_actions');
+  await expect(link).toHaveAttribute(
+    'href',
+    'https://juno.build/docs/guides/github-actions'
+  );
 });
 
-test('get discord link', async ({ page }) => {
+test('get discord link', async ({page}) => {
   await page.goto('/');
 
-  const link = page.locator('a[aria-label="Join Juno\'s Discord channel for questions or to share the fun"]');
+  const link = page.locator(
+    'a[aria-label="Join Juno\'s Discord channel for questions or to share the fun"]'
+  );
 
   await expect(link).toHaveAttribute('href', 'https://discord.gg/wHZ57Z2RAG');
 });
