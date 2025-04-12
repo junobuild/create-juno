a03ba2df3435898b625bc00d25139fbe7af59638
David Dal Busco <david.dalbusco@outlook.com>
2025-04-12 09:58:26 +0200

    feat: revert and redo banner, it's less intrusive

--- templates/react-ts-example/src/App.tsx
+++ templates/react-ts-example/src/App.tsx
@@ -1,15 +1,13 @@
 import { initSatellite } from "@junobuild/core";
-import { FC, useEffect, useState } from "react";
-import { Onboarding } from "./components/Onboarding";
+import { FC, useEffect } from "react";
 import { Auth } from "./components/Auth";
-import { Table } from "./components/Table";
-import { Modal } from "./components/Modal";
 import { Background } from "./components/Background";
 import { Footer } from "./components/Footer";
+import { Modal } from "./components/Modal";
+import { Table } from "./components/Table";
+import { Banner } from "./components/Banner.tsx";
 
 const App: FC = () => {
-  const [onboardingCompleted, setOnboardingCompleted] = useState(false);
-
   useEffect(() => {
     (async () =>
       await initSatellite({
@@ -20,44 +18,39 @@ const App: FC = () => {
   }, []);
 
   return (
-    <div className="relative isolate min-h-[100dvh]">
-      <main className="mx-auto max-w-(--breakpoint-2xl) px-8 py-16 md:px-24 [@media(min-height:800px)]:min-h-[calc(100dvh-128px)]">
-        <Onboarding
-          onComplete={(completed: boolean) => setOnboardingCompleted(completed)}
-        />
+    <>
+      <div className="relative isolate min-h-[100dvh]">
+        <Banner />
 
-        {(onboardingCompleted || !import.meta.env.DEV) && (
-          <>
-            <h1 className="text-5xl font-bold tracking-tight md:pt-24 md:text-6xl dark:text-white">
-              Example App
-            </h1>
+        <main className="mx-auto max-w-(--breakpoint-2xl) px-8 py-16 md:px-24 [@media(min-height:800px)]:min-h-[calc(100dvh-128px)]">
+          <h1 className="text-5xl font-bold tracking-tight md:pt-24 md:text-6xl dark:text-white">
+            Example App
+          </h1>
+          <p className="py-4 md:max-w-lg dark:text-white">
+            Explore this demo app built with React, Tailwind, and{" "}
+            <a
+              href="https://juno.build"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="underline"
+            >
+              Juno
+            </a>
+            , showcasing a practical application of these technologies.
+          </p>
 
-            <p className="py-4 md:max-w-lg dark:text-white">
-              Sign-in to explore this demo app built with React, Tailwind, and{" "}
-              <a
-                href="https://juno.build"
-                rel="noopener noreferrer"
-                target="_blank"
-                className="underline"
-              >
-                Juno
-              </a>
-              .
-            </p>
+          <Auth>
+            <Table />
 
-            <Auth>
-              <Table />
+            <Modal />
+          </Auth>
+        </main>
 
-              <Modal />
-            </Auth>
-          </>
-        )}
-      </main>
+        <Footer />
 
-      <Footer />
-
-      <Background />
-    </div>
+        <Background />
+      </div>
+    </>
   );
 };
 
