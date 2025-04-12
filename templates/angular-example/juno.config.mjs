b925a2bde23f8a1e0b8d89402bfd926f9892c5a5
David Dal Busco <david.dalbusco@outlook.com>
2025-04-12 19:19:10 +0200

    chore: dev and prod

--- templates/angular-example/juno.config.mjs
+++ templates/angular-example/juno.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "@junobuild/config";
 /** @type {import('@junobuild/config').JunoConfig} */
 export default defineConfig({
   satellite: {
-    id: "<PROD_SATELLITE_ID>",
+    ids: {
+      development: '<DEV_SATELLITE_ID>',
+      production: '<PROD_SATELLITE_ID>'
+    },
     source: "dist/angular-example/browser",
     predeploy: ["npm run build"],
   },
