f8734b030a0884c80d7011121eeff91c106ce73c
David Dal Busco <david.dalbusco@outlook.com>
2025-06-26 08:44:30 +0300

    chore: fmt

--- templates/nextjs-starter/juno.config.mjs
+++ templates/nextjs-starter/juno.config.mjs
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "@junobuild/config";
 
 /** @type {import('@junobuild/config').JunoConfig} */
@@ -6,7 +5,7 @@ export default defineConfig({
   satellite: {
     ids: {
       development: "<DEV_SATELLITE_ID>",
-      production: '<PROD_SATELLITE_ID>'
+      production: "<PROD_SATELLITE_ID>",
     },
     source: "out",
     predeploy: ["npm run build"],
