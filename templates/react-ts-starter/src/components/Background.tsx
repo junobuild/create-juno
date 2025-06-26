b27ea6e442ffb502519bb373f41057c16a397442
David Dal Busco <david.dalbusco@outlook.com>
2025-06-26 08:49:28 +0300

    chore: fmt

--- templates/react-ts-starter/src/components/Background.tsx
+++ templates/react-ts-starter/src/components/Background.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const Background: FC = () => {
   return (
-    <div className="fixed top-[400px] right-0 -z-10 hidden aspect-square h-[100dvh] min-h-[800px] -translate-y-2/4 translate-x-2/4 sm:block lg:translate-x-1/3 [@media(min-height:800px)]:top-2/4">
+    <div className="fixed top-[400px] right-0 -z-10 hidden aspect-square h-[100dvh] min-h-[800px] translate-x-2/4 -translate-y-2/4 sm:block lg:translate-x-1/3 [@media(min-height:800px)]:top-2/4">
       <img
         src="./juno_illustration.svg"
         role="presentation"
