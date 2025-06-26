1c267adc5d375a92eab89bb32e9a6ba36e9ed1ca
David Dal Busco <david.dalbusco@outlook.com>
2025-06-26 08:52:54 +0300

    chore: production id in vanilla juno config template

--- templates/vanilla-js-example/juno.config.mjs
+++ templates/vanilla-js-example/juno.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
   satellite: {
     ids: {
       development: "<DEV_SATELLITE_ID>",
+      production: "<PROD_SATELLITE_ID>",
     },
     source: "dist",
     predeploy: ["npm run build"],
