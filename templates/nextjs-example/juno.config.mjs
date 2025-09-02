beaf08a55807a12e0654c85451f2f49984a64d57
David Dal Busco <david.dalbusco@outlook.com>
2025-09-02 16:03:54 +0200

    chore: reset as on main

--- templates/nextjs-example/juno.config.mjs
+++ templates/nextjs-example/juno.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "@junobuild/config";
 export default defineConfig({
   satellite: {
     ids: {
-      development: "uxrrr-q7777-77774-qaaaq-cai",
+      development: "<DEV_SATELLITE_ID>",
       production: "<PROD_SATELLITE_ID>",
     },
     source: "out",
