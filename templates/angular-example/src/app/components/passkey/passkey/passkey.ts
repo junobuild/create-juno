8fce4d41f663b3ef5714bb095d1869b667cd2456
David Dal Busco <david.dalbusco@outlook.com>
2025-09-05 11:46:51 +0200

    feat: passkey implementation in Angular template

---
+++ templates/angular-example/src/app/components/passkey/passkey/passkey.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-passkey',
+  imports: [],
+  templateUrl: './passkey.html',
+  styleUrl: './passkey.scss'
+})
+export class Passkey {
+
+}
---
+++ templates/angular-example/src/app/components/passkey/use-passkey/use-passkey.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-use-passkey',
+  imports: [],
+  templateUrl: './use-passkey.html',
+  styleUrl: './use-passkey.scss'
+})
+export class UsePasskey {
+
+}
