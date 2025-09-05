73f515f72e65f567b1b44fbe73fb8395f6a91aad
David Dal Busco <david.dalbusco@outlook.com>
2025-09-05 15:20:26 +0200

    feat: passkey for Svelte (#176)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

---
+++ templates/sveltekit-example/src/lib/types/passkey.ts
@@ -0,0 +1,19 @@
+import {
+	WebAuthnSignInProgressStep,
+	type SignProgress,
+	WebAuthnSignUpProgressStep
+} from '@junobuild/core';
+
+export type PasskeyProgress =
+	| {
+			signUp: SignProgress<WebAuthnSignUpProgressStep>;
+	  }
+	| {
+			signIn: SignProgress<WebAuthnSignInProgressStep>;
+	  }
+	| { setup: null };
+
+export interface PasskeyProps {
+	progress: PasskeyProgress | undefined;
+	onProgress: (progress: PasskeyProgress | undefined) => void;
+}
