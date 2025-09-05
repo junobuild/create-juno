1df33b181f17c9497ec8e7f6f5070a9a10ca6093
David Dal Busco <david.dalbusco@outlook.com>
2025-09-05 10:35:23 +0200

    feat: bump latest Juno Core libs and adapt signIn (#168)

--- templates/vue-example/src/components/Login.vue
+++ templates/vue-example/src/components/Login.vue
@@ -2,11 +2,13 @@
 import { signIn } from '@junobuild/core'
 import Button from '@/components/Button.vue'
 
-const login = async () => {
-  await signIn()
-}
+const signInWithII = async () => {
+  await signIn({
+    internet_identity: {},
+  });
+};
 </script>
 
 <template>
-  <Button @click="login">Sign in</Button>
+  <Button @click="signInWithII">Sign in</Button>
 </template>
