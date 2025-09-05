bdd7425a3141b3073f5ee22cd5cd6cac0420b634
David Dal Busco <david.dalbusco@outlook.com>
2025-09-05 16:23:55 +0200

    fix: vanilla display
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- templates/vanilla-js-example/src/components/login.js
+++ templates/vanilla-js-example/src/components/login.js
@@ -15,11 +15,15 @@ export const renderLogin = (app) => {
     fn: signInWithII,
   });
 
-  app.innerHTML = `<div class="gap flex flex-col">
+  app.innerHTML = `<div class="gap flex flex-col w-fit">
     ${renderPasskey(app)}
     
+    <p class="dark:text-white pt-6">
+      Rather like a third party provider?
+    </p>
+    
     <button id="login"
-      class="flex items-center gap-2 border-black dark:border-lavender-blue-500 border-[3px] transition-all rounded-xs py-1 px-8 my-2 font-semibold text-white bg-lavender-blue-500 dark:bg-black shadow-[5px_5px_0px_rgba(0,0,0,1)] dark:shadow-[5px_5px_0px_#7888ff] hover:bg-lavender-blue-600 dark:hover:bg-lavender-blue-300 dark:hover:text-black active:bg-lavender-blue-400 dark:active:bg-lavender-blue-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]">
+      class="flex w-fit  items-center gap-2 border-black dark:border-lavender-blue-500 border-[3px] transition-all rounded-xs py-1 px-8 my-2 font-semibold text-white bg-lavender-blue-500 dark:bg-black shadow-[5px_5px_0px_rgba(0,0,0,1)] dark:shadow-[5px_5px_0px_#7888ff] hover:bg-lavender-blue-600 dark:hover:bg-lavender-blue-300 dark:hover:text-black active:bg-lavender-blue-400 dark:active:bg-lavender-blue-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]">
       Continue with Internet Identity
     </button>   
   </div>`;
