bf5d5cf568771b7bade5762cfbb73ab8a30f3045
David Dal Busco <david.dalbusco@outlook.com>
2025-09-16 08:48:46 +0200

    chore: remove vite-plugin-node-polyfills in templates
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- templates/vue-starter/vite.config.ts
+++ templates/vue-starter/vite.config.ts
@@ -3,12 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import juno from '@junobuild/vite-plugin'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), nodePolyfills(), juno(), tailwindcss()],
+  plugins: [vue(), vueJsx(), juno(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
