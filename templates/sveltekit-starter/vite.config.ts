2b97b9c11c6ee2bad2087641c982dc52dbe961f1
David Dal Busco <david.dalbusco@outlook.com>
2025-09-16 09:25:46 +0200

    chore: remove vite-plugin-node-polyfills in templates (#194)
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- templates/sveltekit-starter/vite.config.ts
+++ templates/sveltekit-starter/vite.config.ts
@@ -5,21 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), juno(), tailwindcss()],
-	optimizeDeps: {
-		esbuildOptions: {
-			define: {
-				global: 'globalThis'
-			},
-			plugins: [
-				{
-					name: 'fix-node-globals-polyfill',
-					setup(build) {
-						build.onResolve({ filter: /_virtual-process-polyfill_\.js/ }, ({ path }) => ({ path }));
-					}
-				}
-			]
-		}
-	},
 	worker: {
 		plugins: () => [sveltekit()],
 		format: 'es'
--- templates/vanilla-js-example/vite.config.js
+++ templates/vanilla-js-example/vite.config.js
@@ -1,9 +1,8 @@
 import juno from "@junobuild/vite-plugin";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tailwindcss from "@tailwindcss/vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  plugins: [nodePolyfills(), juno(), tailwindcss()],
+  plugins: [juno(), tailwindcss()],
 });
