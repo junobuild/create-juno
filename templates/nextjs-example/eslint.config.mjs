c547c6d628be62b03f9750660011a52ffdb12565
David Dal Busco <david.dalbusco@outlook.com>
2025-10-23 22:03:11 +0200

    feat: migrate Next templates to v16
    Signed-off-by: David Dal Busco <david.dalbusco@outlook.com>

--- templates/nextjs-example/eslint.config.mjs
+++ templates/nextjs-example/eslint.config.mjs
@@ -1,27 +1,21 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-      ".prettierrc.js",
-      "public/workers/auth.worker.js",
-    ],
-  },
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    ".prettierrc.js",
+    "public/workers/auth.worker.js",
+  ],
+}];
 
 export default eslintConfig;
